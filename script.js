8505d5ad87df96a1a7f96fcd86312d3c90e96112
ItVang <bigbossluong@gmail.com>
2025-12-17 17:32:47 -0600

    added buttons for each option

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 var humanScore = 0;
 var computerScore = 0;
+var maxRounds = 5;
+var rounds = 1;
+
+const display = document.getElementById("display");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+
+function playGame(input){
+    if(rounds === maxRounds){
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        declareWinner();
+    }
+    else{
+        playRound(input, getComputerChoice());
+        rounds++;
+    }
+}
+
+function declareWinner(){
+    if(humanScore == computerScore){
+        console.log("The game is tied!");
+        display.innerHTML = "The game is tied!";
+
+    }
+    else if(humanScore > computerScore){
+        console.log("You Won!");
+        display.innerHTML = "You Won!";
+    }
+    else{
+        console.log("You Lost!");
+        display.innerHTML = "You Lost!";
+    }
+}
+
+rock.addEventListener("click", (e) => playGame("rock"));
+paper.addEventListener("click", (e) => playGame("paper")) ;
+scissors.addEventListener("click", (e) => playGame("scissors")); 
+
+
+
 
 
 function getComputerChoice(){
@@ -15,65 +59,80 @@ function getComputerChoice(){
     }
 }
 
+
+/*
 function getHumanChoice(){
     return prompt("Rock, Paper, or Scissors?");
 }
-
+*/
 function playRound(humanChoice, computerChoice){
     humanChoice = humanChoice.toLowerCase();
 
     if(humanChoice == "rock" && computerChoice == "paper"){
         console.log("You lost the round! " + computerChoice + " beats " + humanChoice + ".")
+        display.innerHTML = "You lost the round! " + computerChoice + " beats " + humanChoice + ".";
+
         computerScore += 1;
     }
     else if(humanChoice == "paper" && computerChoice == "scissors"){
         console.log("You lost the round! " + computerChoice + " beats " + humanChoice + ".")
+        display.innerHTML = ("You lost the round! " + computerChoice + " beats " + humanChoice + ".");
         computerScore += 1;
     }
     else if(humanChoice == "scissors" && computerChoice == "rock"){
         console.log("You Lost the round! " + computerChoice + " beats " + humanChoice + ".")
+        display.innerHTML = "You lost the round! " + computerChoice + " beats " + humanChoice + ".";
         computerScore += 1;
     }
 
     else if(humanChoice == "rock" && computerChoice == "scissors"){
         console.log("You won the round! " + humanChoice + " beats " + computerChoice + ".")
+        display.innerHTML = "You won the round! " + humanChoice + " beats " + computerChoice + ".";
         humanScore += 1;
     }
     else if(humanChoice == "paper" && computerChoice == "rock"){
         console.log("You won the round! " + humanChoice + " beats " + computerChoice + ".")
+        display.innerHTML = "You won the round! " + humanChoice + " beats " + computerChoice + ".";
         humanScore += 1;
     }
     else if(humanChoice == "scissors" && computerChoice == "paper"){
         console.log("You won the round! " + humanChoice + " beats " + computerChoice + ".")
+        display.innerHTML = "You won the round! " + humanChoice + " beats " + computerChoice + ".";
         humanScore += 1;
     }
 
     else{
         console.log("Tie!");
+        display.innerHTML = "Tie!";
     }
     
 }
 
+/*
 function playGame(){
     let round = 0;
     while (round < 5){
-    let computerPlay= getComputerChoice();
+    let computerPlay = getComputerChoice();
     let humanPlay = getHumanChoice();
         playRound(humanPlay, computerPlay);
         round++;
     }
     if(humanScore == computerScore){
-        console.log("The game is tied!")
+        console.log("The game is tied!");
+        display.innerHTML = "The game is tied!";
+
     }
     else if(humanScore > computerScore){
         console.log("You Won!");
+        display.innerHTML = "You Won!";
     }
     else{
         console.log("You Lost!");
+        display.innerHTML = "You Lost!";
     }
 }
 
 playGame();
-
+*/
 
 
